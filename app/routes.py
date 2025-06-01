5d26db854160f7d1c8f1bdd1fc2e417fdd3c3c5c
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2025-06-01 18:22:27 +0000

    Create the basic templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Drugs"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'drug': {'username': 'Asprin'},
+            'content': 'Aspirin is a medication used to reduce pain, fever, or inflammation.'
+        },
+        {
+            'drug': {'username': 'Ibuprofen'},
+            'content': 'Ibuprofen is a nonsteroidal anti-inflammatory drug (NSAID) used to reduce fever, pain, and inflammation.'
+        },
+        {
+            'drug': {'username': 'Paracetamol'},
+            'content': 'Paracetamol, also known as acetaminophen, is a medication used to treat pain and fever.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
